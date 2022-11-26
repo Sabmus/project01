0edc980e1e7441bf6561f535fa2a6ed234051027
sabmus <sabmus8@gmail.com>
2022-11-26 14:26:28 -0300

    added gunicorn and nginx

--- README.md
+++ README.md
@@ -1,9 +1,17 @@
-## django project01
-After installing python 3.7+
+# django project01
 
-#### Virtualenv
-pip install virtualenv
-virtualenv env
+### Commands
 
-#### Dependencies
-pip install -r requirements.txt
+to run: `docker-compose up --build` (add `-d` for detached)   
+to down: `docker-compose down`
+
+to list images: `docker image ls`   
+to eliminate the images: `docker rmi <image_id>` or `docker image rm <image_id>`   
+
+to list containes: `docker ps -a`   
+to list running containers: `docker ps`   
+
+to list network: `docker network ls`   
+to list volumes: `docker volume ls`   
+
+to enter a container with a bash console: `docker exec -it <container_id> bash`
--- src/project01/settings.py
+++ src/project01/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,12 +21,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-8w+3$++jnegns#mka&vf%x(o5b2=&yde3qbe@yy!=!a(14c#lc'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv('DEBUG')
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -117,7 +118,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_ROOT = '/static/'
+STATIC_URL = '/static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
